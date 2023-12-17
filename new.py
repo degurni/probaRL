7e590eb239a7fd165abdf15a85f66762aa315f46
deg <degurni@rambler.ru>
2023-12-18 01:00:22 +0300

    ok

---
+++ new.py
@@ -0,0 +1,19 @@
+
+# https://github.com/sergio12S/youtoube/tree/master
+# 1 https://www.youtube.com/watch?v=Dwnzhk34vhM
+# 2 https://www.youtube.com/watch?v=_wZoyDdbK6M&list=PLRoQmvSf6MgwTuiZ8BDVCf8GBQ_Szt6Ci&index=3
+# 3 https://www.youtube.com/watch?v=GZ6vsai8T0M&list=PLRoQmvSf6MgwTuiZ8BDVCf8GBQ_Szt6Ci&index=4
+
+from envirenment import Environment
+from dataset import Dataset
+
+
+
+# f = Dataset().get_datas(50, ['BTCUSDT', 'ETHUSDT'], '15m')
+
+
+env = Environment('BTCUSDT', ['close'], 30)
+
+print(env.reset())
+
+print(env.bar)
