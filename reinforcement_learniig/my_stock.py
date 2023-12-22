fafe1c491a4f6236774050305caef82c441f5185
deg <degurni@rambler.ru>
2023-12-22 14:59:13 +0300

    stock

--- reinforcement_learniig/my_stock.py
+++ reinforcement_learniig/my_stock.py
@@ -1,4 +1,5 @@
 
+from dataset import Dataset
 import gymnasium as gym
 import gym_anytrading
 
@@ -9,4 +10,10 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+
+# Импортируем данные о свечах с биржи, формируем датафрейм и записываем в файл
+# data = Dataset().get_data(days=90, symbol='BTCUSDT', tf='5m')
+# data.to_csv('data_BTCUSDT_90d_5m')
+df = pd.read_csv('data_BTCUSDT_90d_5m').set_index('Date')
+print(df)
 
