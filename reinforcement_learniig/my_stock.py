84565829e159b1bc7c8e2210d6852dd7ba1eb228
deg <degurni@rambler.ru>
2023-12-22 14:36:15 +0300

    stock

---
+++ reinforcement_learniig/my_stock.py
@@ -0,0 +1,12 @@
+
+import gymnasium as gym
+import gym_anytrading
+
+from stable_baselines3.common.vec_env import DummyVecEnv  #pip install stable-baselines3[extra]
+from stable_baselines3 import A2C, PPO
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
